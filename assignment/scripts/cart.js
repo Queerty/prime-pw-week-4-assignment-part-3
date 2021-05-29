c470102d0df9fbc621dc0b75697bfe5bb0bf8c19
Meghan Cleland <mplscleland@gmail.com>
2021-05-29 11:16:33 -0700

    fixed empty function

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -7,21 +7,30 @@ function addItem(item, array){
   array.push(item);
   return true;
 } //end addItem
-console.log(addItem(1, basket));
-console.log(`Basket is now ${basket}, it should say [1] so everything worked, right?`);
-console.log(addItem(4, basket));
-console.log(basket);
+console.log(`Adding apples, expect true, ${addItem('apples', basket)}`);
+console.log(`Basket is now: ${basket}, it should say 'apples' so everything worked, right?`);
+console.log(`Accidentally added some 'Coors Light' to my basket: ${addItem('Coors Light', basket)}`);
+console.log(`Now it should say apples and Coors Light: ${basket}`);
 
 function listItems(array){
   for(let x of array){
     console.log(x);
-  }
-}
-listItems(basket);
+
+  }//end of for loop
+}//end of listItems function
+
+addItem('bubble gum', basket);
+console.log(`I will be adding bubble gum to the basket, should read apples, coors, bubble gum: ${basket}`);
+
+console.log(`Now this should list apples, Coors, and bubble gum each on their own line: ${listItems(basket)}`);
 
 function empty(array){
-  return array = [];
-}
+  array.length = 0;
+  return array;
+}// originally I had set array = [], and it worked in the function, but then
+//when I logged the array again it was returning the original array
+//ended up on Stack Overflow and they revealed it has to be written a different way
+empty(basket);
 console.log(empty(basket)); //does return an empty basket...
 
 console.log(basket);// doesnt remain empty... hmmmmm...
